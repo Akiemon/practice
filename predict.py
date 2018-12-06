650dc3a69bf386aa7a8a337a248592fe3635bfa4
Akihiko Okimura <git.euraka.suntory@gmail.com>
2018-12-06 11:38:07 +0900

    データを読み込んで単回帰分析
    データを使って単回帰分析を行う学習をしたため

--- predict.py
+++ predict.py
@@ -43,3 +43,51 @@ import matplotlib.pyplot as plt
 # 横軸x, 縦軸yを散布図（scatter）でプロット
 plt.scatter(x, y)
 plt.show()
+
+# 中心化
+df_c = df - df.mean()
+# データの抽出
+x = df_c["x"]
+y = df_c["y"]
+
+plt.scatter(x, y)
+plt.show()
+
+### パラメータ a の計算 ###
+xx = x * x
+xy = x * y
+a = xy.sum() / xx.sum()
+
+plt.scatter(x, y, label="y") #実測値
+plt.plot(x, a*x, label="y_hat", color="red")#予測値
+plt.legend() #凡例の表示
+plt.show()
+
+### 予測値の計算（部屋の広さが40平米だったら？） ###
+
+x_new = 40 #40平米の部屋
+mean = df.mean()
+mean["x"]
+#中心化
+xc = x_new - mean["x"]
+#単回帰分析による予測
+yc = a*xc
+# 予測値の計算
+y_hat = yc + mean["y"]
+
+### せっかくなんで関数にしてみた ###
+
+def predict(x):
+    #定数項（今回はわかりやすさをとって算出した数値をそのまま代入しています）
+    a = 10069.022519284063
+    xm = 37.62222
+    ym = 121065.0
+    #中心化
+    xc = x -xm
+    #予測値の計算
+    y_hat = a*xc + ym
+    #出力
+    return y_hat
+
+result = predict(50)
+print("家賃の相場は"+ str(result)+ "円です")
